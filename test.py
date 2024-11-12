2591b5987d8a1fd3c16a3381b1facc1297237835
Kuperan25 <kuperanp25@gmail.com>
2024-11-12 18:14:47 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+squares= [x**2 for x in range (10)]
+print (squares)
+z=[]
+for x in range (0-10):
+          z.append(x**2)
+print =z
+
+
+square = lambda x: x**2
+print (square(5))
+yt = youtube 
